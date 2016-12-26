144769a5fbd9eb080739e4a51ac46ba34eb59c9a
ChrisKaspersky <fckthscty@gmail.com>
2016-12-27 01:05:48 +0300

    V 0.0.2 Добавил обработку ошибок

--- Main.py
+++ Main.py
@@ -1,33 +1,43 @@
-# Ввод таблицы
-print("Table path:")
-tpath = input()
-table = open(tpath).read()
-# Массивы под таблицу
-Decoded = []
-Encoded = []
-# Заполнение массивов
-for i in range(table.count('\n')):
-    Decoded.append(table[:table.find('=')-1])
-    table = table[table.find('=')+2:]
-    Encoded.append(table[:table.find('\n')])
-    table = table[table.find('\n')+1:]
-# Ввод файла
-print("File path:")
-fpath = input()
-file = open(fpath).read()
-# Выбор нужного действия
-print("Encode/Decode:")
-methd = input()
-# Выполнение метода
-if methd == "Decode":
-    for i in range(1,len(Encoded)+1):
-        file = file.replace(Encoded[-i],Decoded[-i])
-elif methd == "Encode":
-    for i in range(1,len(Encoded)+1):
-        file = file.replace(Decoded[-i],Encoded[-i])
-else:
-    print("Unknown method")
-# Файловый вывод
-fileout = open(fpath,'w')
-fileout.write(file)
-fileout.close()
+while True:
+    # Ввод таблицы
+    print("Table path:")
+    tpath = input()
+    try :
+        table = open(tpath).read()
+    except FileNotFoundError:
+        print("Ошибка: таблица не найдена")
+        break
+    # Массивы под таблицу
+    Decoded = []
+    Encoded = []
+    # Заполнение массивов
+    for i in range(table.count('\n')):
+        Decoded.append(table[:table.find('=')-1])
+        table = table[table.find('=')+2:]
+        Encoded.append(table[:table.find('\n')])
+        table = table[table.find('\n')+1:]
+    # Ввод файла
+    print("File path:")
+    fpath = input()
+    try :
+        file = open(fpath).read()
+    except FileNotFoundError:
+        print("Ошибка: файл не найден")
+        break
+    # Выбор нужного действия
+    print("Encrypt/Decrypt:")
+    methd = input()
+    # Выполнение метода
+    if methd == "Decrypt":
+        for i in range(1,len(Encoded)+1):
+            file = file.replace(Encoded[-i],Decoded[-i])
+    elif methd == "Encrypt":
+        for i in range(1,len(Encoded)+1):
+            file = file.replace(Decoded[-i],Encoded[-i])
+    else:
+        print("Ошибка: неизвестное действие")
+        break
+    # Файловый вывод
+    fileout = open(fpath,'w')
+    fileout.write(file)
+    fileout.close()
