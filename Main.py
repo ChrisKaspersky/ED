1ad8a155aef91c7c2a48b1190808fd0b8374a095
ChrisKaspersky <fckthscty@gmail.com>
2016-12-27 23:45:56 +0300

    Сделал программу модульной

--- Main.py
+++ Main.py
@@ -1,58 +1,12 @@
-while True:  # Для возможности при ошибке остановить программу с помощью break
-    # Ввод пути таблицы
-    print("Table path:")
-    tpath = input()
-    # Проверка корректности ввода пути таблицы
-    try :
-        table = open(tpath).read()
-    except FileNotFoundError:  # Если файл не найден
-        print("Ошибка: таблица не найдена")
+import os
+print("Select action \n- Encrypt with table \n- Encrypt with hash algorithm \n")
+while True:
+    action = input().lower()
+    if action == "encrypt with table":
+        os.system("table_coding.py")
         break
-    except IsADirectoryError:  # Введён некорректный путь
-        print("Ошибка: введён путь к директории, а не файлу")
+    elif action == "encrypt with hash algorithm":
+        os.system("md5.py")
         break
-    except PermissionError:  # Недостаточно прав для доступа к файлу\директории
-        print("Ошибка: недостаточно прав для доступа")
-        break
-    # Массивы под таблицу
-    Decoded = []
-    Encoded = []
-    # Заполнение массивов
-    for i in range(table.count('\n')):
-        Decoded.append(table[:table.find('=')-1])
-        table = table[table.find('=')+2:]
-        Encoded.append(table[:table.find('\n')])
-        table = table[table.find('\n')+1:]
-    # Ввод пути файла
-    print("File path:")
-    fpath = input()
-    # Проверка корректности ввода пути к файлу
-    try :
-        file = open(fpath).read()
-    except FileNotFoundError:
-        print("Ошибка: файл не найден")  # Если файл не найден
-        break
-    except IsADirectoryError:  # Введён некорректный путь
-        print("Ошибка: введён путь к директории, а не файлу")
-        break
-    except PermissionError:  # Недостаточно прав для доступа к файлу\директории
-        print("Ошибка: недостаточно прав для доступа")
-        break
-    # Выбор нужного действия
-    print("Encrypt/Decrypt:")
-    methd = input()
-    # Выполнение метода
-    if methd == "Decrypt":
-        for i in range(1,len(Encoded)+1):
-            file = file.replace(Encoded[-i],Decoded[-i])
-    elif methd == "Encrypt":
-        for i in range(1,len(Encoded)+1):
-            file = file.replace(Decoded[-i],Encoded[-i])
     else:
-        print("Ошибка: неизвестное действие")
-        break
-    # Файловый вывод
-    fileout = open(fpath,'w')
-    fileout.write(file)
-    fileout.close()
-    break
+        print("Please, select something from the proposed")
