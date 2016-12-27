e33d4845c3f87fb2d4df151714909ae61af4eabe
ChrisKaspersky <fckthscty@gmail.com>
2016-12-27 15:11:51 +0300

    Добавил обработку некоторых ошибок

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
-while True:
-    # Ввод таблицы
+while True:  # Для возможности при ошибке остановить программу с помощью break
+    # Ввод пути таблицы
     print("Table path:")
     tpath = input()
+    # Проверка корректности ввода пути таблицы
     try :
         table = open(tpath).read()
-    except FileNotFoundError:
+    except FileNotFoundError:  # Если файл не найден
         print("Ошибка: таблица не найдена")
+        break
+    except IsADirectoryError:  # Введён некорректный путь
+        print("Ошибка: введён путь к директории, а не файлу")
+        break
+    except PermissionError:  # Недостаточно прав для доступа к файлу\директории
+        print("Ошибка: недостаточно прав для доступа")
         break
     # Массивы под таблицу
     Decoded = []
@@ -16,13 +23,20 @@ while True:
         table = table[table.find('=')+2:]
         Encoded.append(table[:table.find('\n')])
         table = table[table.find('\n')+1:]
-    # Ввод файла
+    # Ввод пути файла
     print("File path:")
     fpath = input()
+    # Проверка корректности ввода пути к файлу
     try :
         file = open(fpath).read()
     except FileNotFoundError:
-        print("Ошибка: файл не найден")
+        print("Ошибка: файл не найден")  # Если файл не найден
+        break
+    except IsADirectoryError:  # Введён некорректный путь
+        print("Ошибка: введён путь к директории, а не файлу")
+        break
+    except PermissionError:  # Недостаточно прав для доступа к файлу\директории
+        print("Ошибка: недостаточно прав для доступа")
         break
     # Выбор нужного действия
     print("Encrypt/Decrypt:")
@@ -41,3 +55,4 @@ while True:
     fileout = open(fpath,'w')
     fileout.write(file)
     fileout.close()
+    break
