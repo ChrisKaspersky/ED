f2caadc615b8818fe0cf4bfb4133b4acf12237cd
ChrisKaspersky <fckthscty@gmail.com>
2016-12-28 23:17:38 +0300

    Добавил возможность шифрования по пользовательскому ключу

--- rsa.py
+++ rsa.py
@@ -16,24 +16,49 @@ while True:
         break
     print("Encrypt or decrypt?")
     while True:
-        selection = input()
-        selection = selection.lower()
+        selection = input().lower()
         if selection == "encrypt":
-            # Генерация необходимых переменных для RSA
-            p = rsa_service.rsa_keygen_p()
-            q = rsa_service.rsa_keygen_q()
-            n = rsa_service.rsa_keygen_n(p,q)
-            eiler = rsa_service.rsa_keygen_eiler(p,q)
-            e = rsa_service.rsa_keygen_e(eiler)
-            d = rsa_service.rsa_keygen_d(eiler,e)
-            # Сохранение открытого ключа
-            print("Enter directory to save rsa public key:")
-            openkey_path = input()
-            openkey_save = open(openkey_path,'w')
-            openkey_save.write(str(e))
-            openkey_save.write(" ")
-            openkey_save.write(str(n))
-            openkey_save.close()
+            print("You alredy have rsa key?")
+            key_selection = input().lower()
+            if key_selection == "no":
+                # Генерация необходимых переменных для RSA
+                p = rsa_service.rsa_keygen_p()
+                q = rsa_service.rsa_keygen_q()
+                n = rsa_service.rsa_keygen_n(p,q)
+                eiler = rsa_service.rsa_keygen_eiler(p,q)
+                e = rsa_service.rsa_keygen_e(eiler)
+                d = rsa_service.rsa_keygen_d(eiler,e)
+                # Запись открытого ключа
+                print("Enter directory to save rsa public key:")
+                openkey_path = input()
+                openkey_save = open(openkey_path,'w')
+                openkey_save.write(str(e))
+                openkey_save.write(" ")
+                openkey_save.write(str(n))
+                openkey_save.close()
+                # Запись секретного ключа
+                print("Enter directory to save rsa private key:")
+                privatekey_path = input()
+                privatekey_save = open(privatekey_path, 'w')
+                privatekey_save.write(str(d))
+                privatekey_save.write(" ")
+                privatekey_save.write(str(n))
+            else:
+                print("Enter path to the public rsa key")
+                key_in_path = input()
+                try:
+                    key_in = open(key_in_path, 'r').read()
+                except FileNotFoundError:  # Если файл не найден
+                    print("Error: file not found")
+                    break
+                except IsADirectoryError:  # Введён некорректный путь
+                    print("Error: entered path to the directory, not to file")
+                    break
+                except PermissionError:  # Недостаточно прав для доступа к файлу\директории
+                    print("Error: you don't have permission to this file\directory")
+                    break
+                e = int(key_in[:key_in.find(" ")])
+                n = int(key_in[key_in.find(" ")+1:])
             # Шифрование сообщения
             fout = ""
             for i in range(len(file)):
@@ -41,13 +66,6 @@ while True:
             # Запись зашифрованного сообщения
             fout_save = open(fpath,'w')
             fout_save.write(fout)
-            # Запись секретного ключа
-            print("Enter directory to save rsa private key:")
-            privatekey_path = input()
-            privatekey_save = open(privatekey_path,'w')
-            privatekey_save.write(str(d))
-            privatekey_save.write(" ")
-            privatekey_save.write(str(n))
             break
         elif selection == "decrypt":
             # Ввод секретного ключа
