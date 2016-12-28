04079f58d2611312b440d1466e786e692f14371d
ChrisKaspersky <fckthscty@gmail.com>
2016-12-28 13:20:35 +0300

    Добавил sha-1

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 import os
-print("Select action \n- Encrypt with table \n- Encrypt with hash algorithm \n")
+print("Select action \n- Encrypt with table \n- Encrypt with md5 hash algorithm \n- Encrypt with SHA hash algorithm")
 while True:
     action = input().lower()
     if action == "encrypt with table":
         os.system("table_coding.py")
         break
-    elif action == "encrypt with hash algorithm":
+    elif action == "encrypt with md5 hash algorithm":
         os.system("md5.py")
+        break
+    elif action == "encrypt with SHA hash algorithm":
+        os.system("sha1.py")
         break
     else:
         print("Please, select something from the proposed")
